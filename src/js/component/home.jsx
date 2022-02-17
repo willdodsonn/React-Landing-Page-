b996aa4ef9c590558353a37cbb85f80024c4ae37
willdodsonn <sodlax38@gmail.com>
2022-02-17 00:36:28 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar.jsx";
-import Jumbotron from "./Jumbotron.jsx"
+import Jumbotron from "./Jumbotron.jsx";
+import 'relative/path/to/stylesheet.scss';
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -8,20 +9,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div>
-			<Navbar/>
-			<Jumbotron/>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
+			<Navbar />
+		
+			<Jumbotron />
 		</div>
 	);
 };
